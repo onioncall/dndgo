38b8e0f30961981f1c531312db9d91b163d71748
onioncall <orion.callaghan@gmail.com>
2025-12-04 20:02:49 -0600

    leaving utf8 implementation in each method

--- tui/shared/utils.go
+++ tui/shared/utils.go
@@ -1,13 +1,8 @@
 package shared
 
-import (
-	"unicode/utf8"
-)
-
 // takes a string and a length. If the string exceeds that length, we truncate and replace the last three letters with an elipses
 func TruncateString(s string, length int) string {
 	runes := []rune(s)
-	// logger.Infof("'%s': String Length: %d, VP Length: %d", s, len(s), length)
 
 	if len(runes) < length {
 		// no truncation needed
@@ -17,9 +12,4 @@ func TruncateString(s string, length int) string {
 	truncatedRunes := runes[:length-3]
 
 	return string(truncatedRunes) + "..."
-}
-
-// Returns utf8 string length. The tui needs it all the time, and it uses too many characters
-func StringLen(s string) int {
-	return utf8.RuneCountInString(s)
 }
