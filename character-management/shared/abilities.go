3ce9b60952fb979de4bacf0519be06f7dce1a1e3
onioncall <orion.callaghan@gmail.com>
2026-01-08 22:52:53 -0600

    added comments around structs fields I often confuse

--- character-management/shared/abilities.go
+++ character-management/shared/abilities.go
@@ -2,9 +2,9 @@ package shared
 
 type Ability struct {
 	Name                   string `json:"name"`
-	Base                   int    `json:"base"`
-	Adjusted               int    `json:"-"`
-	AbilityModifier        int    `json:"-"`
+	Base                   int    `json:"base"` // int 1-20, not to be mutated
+	Adjusted               int    `json:"-"`    // int 1-20, value to mutate in code. Is not persisted
+	AbilityModifier        int    `json:"-"`    // int between -10 and 10. Derived from Adjusted
 	SavingThrowsProficient bool   `json:"saving-throws-proficient" clover:"saving-throws-proficient"`
 }
 
